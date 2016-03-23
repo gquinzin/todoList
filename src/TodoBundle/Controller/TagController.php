19781280c80e5553350f2f29014a4ade87d6c898
gquinzin <gilles.quinzin@hotmail.fr>
2016-03-23 11:50:35 +0100

    tag

---
+++ src/TodoBundle/Controller/TagController.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gilles
+ * Date: 23/03/16
+ * Time: 11:29
+ */
+
+namespace TodoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\httpFoundation\Request;
+use TodoBundle\Entity\Tag;
+use TodoBundle\Form\Type\TagType;
+
+class TagController extends Controller
+{
+    /**
+     * @Route("/tag/create")
+     */
+    public function createAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $tag = new tag();
+        $form = $this->createForm(TagType::class, $tag);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($tag);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Tag added with success'
+            );
+
+            return $this->redirect("/");
+        }
+
+        return $this->render('TodoBundle:Tag:create.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/tag/list")
+     */
+    public function listAction()
+    {
+        $tags = $this->getDoctrine()
+            ->getRepository('TodoBundle:Tag')
+            ->findAll();
+
+        return $this->render('TodoBundle:Tag:list.html.twig', array(
+            'tags' => $tags,
+        ));
+    }
+}
